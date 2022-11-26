59396747e987a751202e9638c109577027768717
Jimmy Sawczuk <me@jimmysawczuk.com>
2022-11-25 19:29:15 -0500

    Add dtable package, bump dependencies and go version

---
+++ aws/dtable/dtable.go
@@ -0,0 +1,125 @@
+package dtable
+
+import (
+	"runtime"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+type Table struct {
+	conn *dynamodb.DynamoDB
+	log  logrus.FieldLogger
+
+	Name string
+}
+
+func New(conn *dynamodb.DynamoDB, name string, log logrus.FieldLogger) *Table {
+	return &Table{
+		conn: conn,
+		log:  log,
+		Name: name,
+	}
+}
+
+func (t *Table) Conn() *dynamodb.DynamoDB {
+	return t.conn
+}
+
+func debug(log logrus.FieldLogger, name string, in any) {
+	if log != nil {
+		_, file, line, _ := runtime.Caller(3)
+		log.WithFields(logrus.Fields{
+			"table": name,
+			"file":  file,
+			"line":  line,
+			// "in": in,
+		}).Debug(name)
+	}
+}
+
+func (t *Table) PutItem(ctx aws.Context, in *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error) {
+	debug(t.log, "PutItem", in)
+
+	in.TableName = aws.String(t.Name)
+	return t.conn.PutItemWithContext(ctx, in)
+}
+
+func (t *Table) GetItem(ctx aws.Context, in *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error) {
+	debug(t.log, "PutItem", in)
+
+	in.TableName = aws.String(t.Name)
+	return t.conn.GetItemWithContext(ctx, in)
+}
+
+type BatchGetItemInput struct {
+	KeysAndAttributes      *dynamodb.KeysAndAttributes
+	ReturnConsumedCapacity *string
+}
+
+type BatchGetItemOutput struct {
+	ConsumedCapacity []*dynamodb.ConsumedCapacity
+	Responses        []map[string]*dynamodb.AttributeValue
+	UnprocessedKeys  map[string]*dynamodb.KeysAndAttributes
+}
+
+type Key struct {
+	PK string
+	SK string
+}
+
+func GetKeysAndAttributes(pk, sk string, keys []Key) []map[string]*dynamodb.AttributeValue {
+	v := make([]map[string]*dynamodb.AttributeValue, len(keys))
+	for i, k := range keys {
+		v[i] = map[string]*dynamodb.AttributeValue{
+			pk: {S: aws.String(k.PK)},
+			sk: {S: aws.String(k.SK)},
+		}
+	}
+	return v
+}
+
+func (t *Table) BatchGetItem(ctx aws.Context, in *BatchGetItemInput) (*BatchGetItemOutput, error) {
+	debug(t.log, "BatchGetItem", in)
+
+	din := dynamodb.BatchGetItemInput{
+		RequestItems: map[string]*dynamodb.KeysAndAttributes{
+			t.Name: in.KeysAndAttributes,
+		},
+		ReturnConsumedCapacity: in.ReturnConsumedCapacity,
+	}
+
+	dout, err := t.conn.BatchGetItemWithContext(ctx, &din)
+	if err != nil {
+		return nil, errors.Wrap(err, "batch get item with context")
+	}
+
+	return &BatchGetItemOutput{
+		ConsumedCapacity: dout.ConsumedCapacity,
+		Responses:        dout.Responses[t.Name],
+		UnprocessedKeys:  dout.UnprocessedKeys,
+	}, nil
+}
+
+func (t *Table) UpdateItem(ctx aws.Context, in *dynamodb.UpdateItemInput) (*dynamodb.UpdateItemOutput, error) {
+	debug(t.log, "UpdateItem", in)
+
+	in.TableName = aws.String(t.Name)
+	return t.conn.UpdateItemWithContext(ctx, in)
+}
+
+func (t *Table) DeleteItem(ctx aws.Context, in *dynamodb.DeleteItemInput) (*dynamodb.DeleteItemOutput, error) {
+	debug(t.log, "DeleteItem", in)
+
+	in.TableName = aws.String(t.Name)
+	return t.conn.DeleteItemWithContext(ctx, in)
+}
+
+func (t *Table) Query(ctx aws.Context, in *dynamodb.QueryInput) (*dynamodb.QueryOutput, error) {
+	debug(t.log, "Query", in)
+
+	in.TableName = aws.String(t.Name)
+	return t.conn.QueryWithContext(ctx, in)
+}
--- go.mod
+++ go.mod
@@ -1,17 +1,25 @@
 module github.com/jimmysawczuk/kit
 
-go 1.16
+go 1.18
 
 require (
-	github.com/aws/aws-lambda-go v1.23.0
-	github.com/aws/aws-sdk-go v1.37.30
+	github.com/aws/aws-lambda-go v1.35.0
+	github.com/aws/aws-sdk-go v1.44.145
 	github.com/go-chi/chi v1.5.4
-	github.com/go-sql-driver/mysql v1.5.0
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/jimmysawczuk/try v1.0.0
-	github.com/jmoiron/sqlx v1.3.1
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/jmoiron/sqlx v1.3.5
 	github.com/pkg/errors v0.9.1
-	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.6.1
+	github.com/sirupsen/logrus v1.9.0
+	github.com/stretchr/testify v1.7.2
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
