d6b3ade32df9198465544b62177e63bf00dfcadf
Jimmy Sawczuk <me@jimmysawczuk.com>
2024-01-02 21:59:59 -0500

    Add ProxyHandler

--- aws/apigateway/apigateway.go
+++ aws/apigateway/apigateway.go
@@ -14,6 +14,63 @@ import (
 	"github.com/pkg/errors"
 )
 
+func ProxyHandler(handler http.Handler) func(events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return func(ar events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		w := &responseWriter{
+			statusCode: 0,
+			header:     http.Header{},
+			buf:        &bytes.Buffer{},
+		}
+
+		r := &http.Request{
+			Method:     ar.RequestContext.HTTPMethod,
+			RequestURI: ar.RequestContext.Path,
+			Proto:      ar.RequestContext.Protocol,
+			Header:     multiValueHeader(ar.Headers),
+		}
+
+		if ar.Body != "" && ar.IsBase64Encoded {
+			r.Body = io.NopCloser(base64.NewDecoder(base64.StdEncoding, strings.NewReader(ar.Body)))
+		} else if ar.Body != "" && !ar.IsBase64Encoded {
+			r.Body = io.NopCloser(strings.NewReader(ar.Body))
+		}
+
+		var ok bool
+		r.ProtoMajor, r.ProtoMinor, ok = http.ParseHTTPVersion(r.Proto)
+		if !ok {
+			return events.APIGatewayProxyResponse{}, errors.Errorf("http: parse http version: couldn't parse version %s", r.Proto)
+		}
+
+		var err error
+		r.URL, err = url.ParseRequestURI(r.RequestURI)
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, errors.Wrap(err, "url: parse request uri")
+		}
+
+		r = r.WithContext(context.Background())
+
+		handler.ServeHTTP(w, r)
+
+		if w.statusCode == 0 {
+			w.statusCode = http.StatusOK
+		}
+
+		resp := events.APIGatewayProxyResponse{
+			StatusCode: w.statusCode,
+			Headers:    singleValueHeader(w.header),
+		}
+
+		if w.isBinary {
+			resp.Body = base64.StdEncoding.EncodeToString(w.buf.Bytes())
+			resp.IsBase64Encoded = true
+		} else {
+			resp.Body = w.buf.String()
+		}
+
+		return resp, nil
+	}
+}
+
 // HTTPHandler shims an http.Handler into a API Gateway V2 request/response workflow.
 func HTTPHandler(handler http.Handler) func(events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	return func(ar events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
