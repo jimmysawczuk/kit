3dd542e97b0c4cc2005ab73c528baf8c70ae6622
Jimmy Sawczuk <jimmy@louddoor.com>
2025-03-06 21:10:27 -0500

    WIP

--- aws/dtable/v2/dtable.go
+++ aws/dtable/v2/dtable.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
@@ -80,6 +81,83 @@ func (t *Table) Put(ctx context.Context, model Model) error {
 	if _, err := t.conn.PutItemWithContext(ctx, &dynamodb.PutItemInput{
 		TableName: aws.String(t.name),
 		Item:      m,
+	}); err != nil {
+		return fmt.Errorf("dynamodb: put item: %w", err)
+	}
+
+	return nil
+}
+
+func (t *Table) Create(ctx context.Context, model Model) error {
+	m, err := dynamodbattribute.MarshalMap(model)
+	if err != nil {
+		return fmt.Errorf("dynamodbattr: marshal: %w", err)
+	}
+
+	m["PK"] = &dynamodb.AttributeValue{S: aws.String(model.PK())}
+	m["SK"] = &dynamodb.AttributeValue{S: aws.String(model.SK())}
+
+	expr, err := expression.NewBuilder().WithCondition(
+		expression.ConditionBuilder(expression.And(
+			expression.AttributeNotExists(expression.Name("PK")),
+			expression.AttributeNotExists(expression.Name("SK")),
+		)),
+	).Build()
+	if err != nil {
+		return fmt.Errorf("expression: build: %w", err)
+	}
+
+	if _, err := t.conn.PutItemWithContext(ctx, &dynamodb.PutItemInput{
+		TableName:                aws.String(t.name),
+		Item:                     m,
+		ExpressionAttributeNames: expr.Names(),
+		ConditionExpression:      expr.Condition(),
+	}); err != nil {
+		return fmt.Errorf("dynamodb: put item: %w", err)
+	}
+
+	return nil
+}
+
+func (t *Table) Update(ctx context.Context, model Model) error {
+	update := expression.Set(expression.Name("UpdatedAt"), expression.Value(time.Now().UTC()))
+	rv := reflect.ValueOf(model)
+	for i := range rv.NumField() {
+		field := rv.Field(i)
+		tag, ok := rv.Type().Field(i).Tag.Lookup("dynamodbav")
+		if ok {
+			if tag == "-" {
+				continue
+			}
+
+			update.Set(expression.Name(tag), expression.Value(field.Interface()))
+			continue
+		}
+
+		update.Set(expression.Name(field.Type().Name()), expression.Value(field.Interface()))
+	}
+
+	expr, err := expression.NewBuilder().WithUpdate(update).
+		WithCondition(
+			expression.And(
+				expression.Equal(expression.Name("PK"), expression.Value(model.PK())),
+				expression.Equal(expression.Name("SK"), expression.Value(model.SK())),
+			),
+		).Build()
+	if err != nil {
+		return fmt.Errorf("expression: build: %w", err)
+	}
+
+	if _, err := t.conn.UpdateItemWithContext(ctx, &dynamodb.UpdateItemInput{
+		TableName: aws.String(t.name),
+		Key: map[string]*dynamodb.AttributeValue{
+			"PK": {S: aws.String(model.PK())},
+			"SK": {S: aws.String(model.SK())},
+		},
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		ConditionExpression:       expr.Condition(),
+		UpdateExpression:          expr.Update(),
 	}); err != nil {
 		return fmt.Errorf("dynamodb: put item: %w", err)
 	}
